2727ff5897383d54c189db353046c615b82b71b5
pstaal <peter.staal@gmail.com>
2021-04-19 14:34:17 +0200

    add top movies to index.js

--- index.js
+++ index.js
@@ -1,2 +1,39 @@
 const express = require('express');
 const app = express();
+
+let topMovies = [
+  {
+    title: 'Midsommar'
+  },
+  {
+    title: 'Lord of the Rings'
+  },
+  {
+    title: 'The Joker'
+  },
+  {
+    title: 'Star Wars'
+  },
+  {
+    title: 'Casino'
+  },
+  {
+    title: 'The Wolf of Wallstreet'
+  },
+  {
+    title: 'Scarface'
+  },
+  {
+    title: 'Her'
+  },
+  {
+    title: 'Wallstreet'
+  },
+  {
+    title: 'Forest Gump'
+  }
+];
+
+app.get('/movies', (req, res) => {
+  res.json(topMovies);
+});
