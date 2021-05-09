28f45421ac019836efa83fde442fe8a31196fe3b
pstaal <peter.staal@gmail.com>
2021-05-09 12:01:34 +0200

    import models and mongoose in index.js

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require('express'),
 morgan = require('morgan'),
 uuid = require('uuid'),
 bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const Models = require('./models.js');
+
+const Movies = Models.Movie;
+const Users = Models.User;
+
+mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true });
 
 const app = express();
 
