4b205cbe89aca6e05314d1eebc38506a63ef334d
pstaal <peter.staal@gmail.com>
2021-05-21 09:13:27 +0200

    add CORS to index.js

--- index.js
+++ index.js
@@ -6,13 +6,26 @@ const mongoose = require('mongoose');
 const Models = require('./models.js');
 const passport = require('passport');
 require('./passport');
+const cors = require('cors');
 
 const Movies = Models.Movie;
 const Users = Models.User;
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
 
 mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true });
 
 const app = express();
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message ), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 app.use(bodyParser.json());
 let auth = require('./auth')(app);
